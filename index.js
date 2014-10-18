5f0a331fae8fb1352e1bfded1d4f3d4a4a023c98
Ilkka Huotari <ilkkah@gmail.com>
2014-10-18 13:00:25 +0000

    server side rendering

--- index.js
+++ index.js
@@ -372,14 +372,19 @@ DynamicHtml.prototype.appendTo = function(parent, context, binding) {
   }
 };
 DynamicHtml.prototype.attachTo = function(parent, node, context) {
-  var start = document.createComment(this.expression);
-  var end = document.createComment(this.ending);
+  var bound = this.expression.meta.bindType !== 'unbound';
+  if (bound) {
+    var start = document.createComment(this.expression);
+    var end = document.createComment(this.ending);
+  }
   var value = getUnescapedValue(this.expression, context);
   var html = this.stringify(value);
-  parent.insertBefore(start, node || null);
+  if (bound) parent.insertBefore(start, node || null);
   node = attachHtml(parent, node, html);
-  parent.insertBefore(end, node || null);
-  updateRange(context, null, this, start, end);
+  if (bound) {
+    parent.insertBefore(end, node || null);
+    updateRange(context, null, this, start, end);
+  }
   return node;
 };
 DynamicHtml.prototype.update = function(context, binding) {
